227c4012b2b95ec9c78fb0d939af22ff5afe4e93
Zheng Yune <13390592073@163.com>
2022-02-21 20:10:30 -0500

    add binary search

---
+++ assignment2/binary_search.py
@@ -0,0 +1,26 @@
+def binary_search(arr, low, high, x):
+
+    # Check base case
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        if arr[mid] == x:  # base case
+            return mid
+
+        if arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+        # Element is not present in the array
+        return -1
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
+x = 10
+print(binary_search(arr, 0, len(arr)-1, x))
